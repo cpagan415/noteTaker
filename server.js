842cd4717ca8cf3c87f456f01353d123834c5e40
Christina Pagan <cpagan415@gmail.com>
2021-07-01 16:55:36 -0400

    created server for api routes

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const  db  = require('./db/db.json')
 
 
 app.listen(PORT, ()=> {
     console.log(`Server on port ${PORT}`);
 })
+
+app.get('/api/notes', (req, res) => {
+    res.json(db);
+  });
