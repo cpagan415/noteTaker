b35ec3714fda7c3a8606cbcdda890b5fcf9041e3
Christina Pagan <cpagan415@gmail.com>
2021-07-02 16:32:49 -0400

    connected index and notes page to front end, note creation works

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const app = express();
 //parsing for incoming data
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use(express.static('public'));
 
  //note creation
 
@@ -26,7 +27,16 @@ app.use(express.json());
  }
 
  //validation for note creation here 
+//front end
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+})
 
+app.get('/notes', (req,res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+})
+
+//back-end api
 app.get('/api/notes', (req, res) => {
     res.json(notes);
   });
