71792cb5f5f28e41462be1ae45841eaa934f452b
Christina Pagan <cpagan415@gmail.com>
2021-07-02 15:59:57 -0400

    set up api post and get routes

--- server.js
+++ server.js
@@ -1,13 +1,44 @@
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
+const { notes }  = require('./db/notes.json');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
-const  db  = require('./db/db.json')
 
+//parsing for incoming data
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
-app.listen(PORT, ()=> {
-    console.log(`Server on port ${PORT}`);
-})
+ //note creation
+
+ function createNote(body, noteArray)
+ {
+    const note = body;
+    noteArray.push(note);
+
+    fs.writeFileSync(
+    
+    path.join(__dirname, './db/notes.json'),
+    JSON.stringify({notes: noteArray}, null, 2)
+    );
+     return note;
+ }
+
+ //validation for note creation here 
 
 app.get('/api/notes', (req, res) => {
-    res.json(db);
+    res.json(notes);
   });
+
+  app.post('/api/notes', (req,res) => {
+      //for id creation
+      req.body.id = notes.length.toString();
+      const note = createNote(req.body, notes);
+
+      res.json(note);
+  })
+
+  app.listen(PORT, ()=> {
+    console.log(`Server on port ${PORT}`);
+})
